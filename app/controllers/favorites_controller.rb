3e5dca45ec2e1d4965016be18940f2f278afb72f
cherifb16 <cherifbobbyray@gmail.com>
2022-05-14 15:00:27 +0200

    github actions test

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+
+  def create
+    @blog = Blog.find(params[:blog_id])
+    favorite = current_user.favorites.new(blog_id: @blog.id)
+    favorite.save
+  end
+  def destroy
+    @blog = Blog.find(params[:blog_id])
+    favorite = current_user.favorites.find_by(blog_id: @blog.id)
+    favorite.destroy
+    #current_user.favorites.find_by(id: params[:id]).destroy
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'blogs#index'
+  resources :blogs do
+    resource :favorites, only: [:create, :destroy]
+  end
+  devise_for :users
+end 
